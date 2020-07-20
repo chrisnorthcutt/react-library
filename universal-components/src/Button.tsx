2e9098c40837cec50f5a3502f163961880f09f44
Kenneth Spry <spry_kenneth@network.lilly.com>
2020-07-20 10:03:19 -0400

    Included other variables

--- universal-components/src/Button.tsx
+++ universal-components/src/Button.tsx
@@ -1,15 +1,46 @@
 import * as React from "react";
-import { colors } from "./colors";
+import {useState} from "react";
+import { colors, sizes } from "./variables";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+interface Props {
+    isPressed: Boolean;
+}
+
+const StyledButton = styled(motion.button).attrs((props: Props) => {
+    return {
+        isPressed: props.isPressed,
+    }
+})`
+    width: ${sizes.auto};
+    height: 4vh;
+    padding: 0.5rem 1rem;
+    background: ${colors.primary400};
+    outline: none;
+    border: none;
+    border-radius: 0.5vh;
+    font-family: 'Helvetica';
+    font-weight: 800;
+    font-size: 1rem;
+    color: ${colors.white};
+    transition: transform 200ms ease-in-out;
+    transform: scale(${props => props.isPressed ? 0.5 : 1});
+    &.big-button {
+        width: ${sizes.full};
+    }
+`
 
 export function Button(props: any) {
-    const buttonStyle = {
-        color: colors["primary-400"],
-        background: colors["grey-200"]
-    }
+    const [isPressed, setPressed] = useState(false)
     return (
-        <button style={buttonStyle}>
+        <StyledButton 
+            onTapStart={() => setPressed(true)}
+            onTap={() => setPressed(false)}
+            isPressed={isPressed}
+            className="big-button">
             Howdy
-        </button>
+        </StyledButton>
     )
 }
 
---
+++ universal-components/src/variables.tsx
@@ -0,0 +1,21 @@
+export const colors = {
+   'warning': '#f69a19',
+   'warningDark': '#c75200',
+   'success': '#45ba7f',
+   'successDark': '#31845b',
+   'danger': '#d52b1e',
+   'dangerDark': '#aa2218',
+   'grey200': '#eeeeee',
+   'grey900': '#212121',
+   'primary900': '#00384d',
+   'primary600': '#0081b3',
+   'grey400': '#bdbdbd',
+   'grey700': '#616161',
+   'primary400': '#00a1de',
+   'white': '#ffffff'
+}
+
+export const sizes = {
+   'auto': 'auto',
+   'full': '90vw'
+}
