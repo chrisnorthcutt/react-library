5ec9555ead69d66a75d806a76194b76923994b65
Chris Northcutt <chris.r.northcutt@gmail.com>
2020-08-06 11:58:53 -0400

    Updated Text Field & Switch

--- universal-prototypes/UF-Core.framerfx/code/TextField.tsx
+++ universal-prototypes/UF-Core.framerfx/code/TextField.tsx
@@ -7,294 +7,307 @@ import * as Type from "./Typography";
 import { Icon } from "./Icon";
 
 interface Props {
-	enabled: Boolean;
-	focused: Boolean;
-	type: String;
+    enabled: Boolean;
+    focused: Boolean;
+    type: String;
 }
 
 const StyledField = styled(motion.div).attrs((props: Props) => {
-	const { enabled, focused, type } = props;
-	return {
-		enabled: enabled,
-		type: type,
-		focused: focused,
-	};
+    const { enabled, focused, type } = props;
+    return {
+        enabled: enabled,
+        type: type,
+        focused: focused,
+    };
 })`
-	width: 100%;
-	height: 100%;
-	opacity: ${(props) => (props.enabled ? 1 : 0.5)};
-	margin-bottom: 1.5rem;
+    width: 100%;
+    height: 100%;
+    opacity: ${(props) => (props.enabled ? 1 : 0.5)};
+    margin-bottom: 1.5rem;
 
-	> .label {
-		position: absolute;
-		left: ${spacing["2x"]};
-		top: ${spacing["2x"]};
-		font-family: Roboto, sans-serif;
-		font-size: 16px;
-		line-height: 19px;
-	}
-	> .input {
-		width: 100%;
-		padding: 0 ${spacing["2x"]};
-		height: ${spacing["6x"]};
-		padding-top: 12px;
-		font-size: 14px;
-		border-radius: 4px;
-		border-style: solid;
-		outline: none;
-		background: ${(props) =>
-			props.enabled ? colors.white : colors.grey200};
+    > .label {
+        position: absolute;
+        left: ${spacing["2x"]};
+        top: ${spacing["2x"]};
+        font-family: Roboto, sans-serif;
+        font-size: 16px;
+        line-height: 19px;
+    }
+    > .input {
+        width: 100%;
+        padding: 0 ${spacing["2x"]};
+        height: ${spacing["6x"]};
+        padding-top: 12px;
+        font-size: 14px;
+        border-radius: 4px;
+        border-style: solid;
+        outline: none;
+        background: ${(props) =>
+            props.enabled ? colors.white : colors.grey200};
 
-		&:invalid {
-			border-color: ${colors.danger};
-		}
-	}
-	> .assistive {
-		margin-top: 0px;
-		height: ${spacing["2x"]};
-		padding: 0 ${spacing["2x"]};
-		padding-top: 4px;
-	}
+        &:invalid {
+            border-color: ${colors.danger};
+        }
+    }
+    > .assistive {
+        margin-top: 0px;
+        height: ${spacing["2x"]};
+        padding: 0 ${spacing["2x"]};
+        padding-top: 4px;
+    }
 `;
 
 export function TextField(props: any) {
-	const {
-		label,
-		enabled,
-		focused,
-		type,
-		empty,
-		defaultValue = "",
-		assistMessage = "Password must contain at least one letter, at least one number, and be longer than six charaters.",
-		errorMessage = "You have included non-alphabetical characters",
-		color,
-		...rest
-	} = props;
-	const [isFocused, setFocused] = useState(focused);
+    const {
+        label,
+        enabled,
+        focused,
+        type,
+        empty,
+        defaultValue = "",
+        assistMessage = "Password must contain at least one letter, at least one number, and be longer than six charaters.",
+        errorMessage = "You have included non-alphabetical characters",
+        color,
+        ...rest
+    } = props;
+    const [isFocused, setFocused] = useState(focused);
 
-	const [value, setValue] = useState(defaultValue);
-	const [isValid, setValid] = useState(false);
+    const [value, setValue] = useState(defaultValue);
+    const [isValid, setValid] = useState(false);
 
-	const [showPassword, setShowPassword] = useState(false);
-	const inputRef = useRef();
+    const [showPassword, setShowPassword] = useState(false);
+    const inputRef = useRef();
 
-	const id = Math.floor(Math.random() * 8888);
-	let showIcon = false;
-	let activeColor, message;
+    const id = Math.floor(Math.random() * 8888);
+    let showIcon = false;
+    let activeColor, message;
 
-	function onChange(e: any) {
-		const value = e.target.value
-		if (props.onChange) props.onChange(value)
+    function onChange(e: any) {
+        const value = e.target.value;
+        if (props.onChange) props.onChange(value);
 
-        setValue(value)
+        setValue(value);
 
         if (props.onValueChange) {
-            props.onValueChange(value)
+            props.onValueChange(value);
         }
-		setValue(value);
-		if (type === "email") {
-			setValid(emailIsValid(value))
+        setValue(value);
+        if (type === "email") {
+            setValid(emailIsValid(value));
+        } else if (type === "text") setValid(textIsValid(value));
+        else if (type === "password") setValid(passwordIsValid(value));
+    }
 
-		}
-		else if (type === "text") setValid(textIsValid(value));
-		else if (type === "password") setValid(passwordIsValid(value));
-	}
+    useEffect(() => {
+        if (emailIsValid(defaultValue)) {
+            setValid(true);
+            if (value != defaultValue || defaultValue != "") {
+                setValue(defaultValue);
+            }
+        }
+    }, [defaultValue]);
 
-	useEffect(() => {
-		if (emailIsValid(defaultValue)) {
-			setValid(true)
-			if (value != defaultValue ||  defaultValue != "") {
-				setValue(defaultValue)
-			}
-		}
-	  }, [defaultValue])
+    function onFocus() {
+        setFocused(true);
+    }
+    function onBlur() {
+        setFocused(false);
+    }
+    function emailIsValid(email: string) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+    function textIsValid(text: string) {
+        return /^[a-zA-Z-. ]*$/.test(text);
+    }
+    function passwordIsValid(password: string) {
+        return /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/.test(
+            password
+        );
+    }
 
-
-	function onFocus() {
-		setFocused(true);
-	}
-	function onBlur() {
-		setFocused(false);
-	}
-	function emailIsValid(email: string) {
-		return /\S+@\S+\.\S+/.test(email);
-	}
-	function textIsValid(text: string) {
-		return /^[a-zA-Z-. ]*$/.test(text);
-	}
-	function passwordIsValid(password: string) {
-		return /^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/.test(
-			password
-		);
-	}
-
-	if (isFocused) {
-		activeColor = color;
-		message = assistMessage;
-	} else if (!isValid && value.length > 0 && !isFocused) {
-		activeColor = colors.danger;
-		message = errorMessage;
+    if (isFocused) {
+        activeColor = color;
+        message = assistMessage;
+    } else if (!isValid && value.length > 0 && !isFocused) {
+        if (errorMessage === "") {
+            activeColor = colors.grey400;
+            message = assistMessage;
+        } else {
+            activeColor = colors.danger;
+            message = errorMessage;
+            showIcon = !showIcon;
+        }
+    } else if ((!isFocused && !empty) || defaultValue != "") {
+        activeColor = colors.grey400;
+        message = assistMessage;
+    } else if (empty && !isFocused) {
 		showIcon = !showIcon;
-	} else if (!isFocused && !empty || defaultValue != "") {
-		activeColor = colors.grey400;
-		message = assistMessage;
-	} else if (empty && !isFocused) {
-		activeColor = colors.danger;
-		message = label + " cannot be empty";
-	}
+        activeColor = colors.danger;
+        message = label + " cannot be empty";
+    }
 
-	const togglePassword = () => {
-		setShowPassword(!showPassword);
-		//@ts-ignore
-		inputRef.current.focus();
-	};
+    const togglePassword = () => {
+        setShowPassword(!showPassword);
+        //@ts-ignore
+        inputRef.current.focus();
+    };
 
+    return (
+        <StyledField enabled={enabled}>
+            <motion.label
+                className="label"
+                htmlFor={"renene" + id}
+                style={{
+                    originX: 0,
+                }}
+                initial={{
+                    scale: 1,
+                    x: 0,
+                    y: 0,
+                    color: activeColor,
+                }}
+                animate={{
+                    scale:
+                        isFocused || value.length > 0 || defaultValue.length > 0
+                            ? 0.65
+                            : 1,
+                    x:
+                        isFocused || value.length > 0 || defaultValue.length > 0
+                            ? 1
+                            : 0,
+                    y:
+                        isFocused || value.length > 0 || defaultValue.length > 0
+                            ? -15
+                            : 0,
+                    color: activeColor,
+                }}
+                transition={{
+                    duration: 0.15,
+                }}
+            >
+                {label}
+            </motion.label>
 
-
-
-
-	return (
-		<StyledField  enabled={enabled}>
-			<motion.label
-				className="label"
-				htmlFor={"renene" + id}
-				style={{
-					originX: 0,
-				}}
-				initial={{
-					scale: 1,
-					x: 0,
-					y: 0,
-					color: activeColor,
-				}}
-				animate={{
-					scale: isFocused || value.length > 0  || defaultValue.length > 0 ? 0.65 : 1,
-					x: isFocused || value.length > 0 || defaultValue.length > 0 ? 1 : 0,
-					y: isFocused || value.length > 0 || defaultValue.length > 0 ? -15 : 0,
-					color: activeColor,
-				}}
-				transition={{
-					duration: 0.15,
-				}}
-			>
-				{ label }
-			</motion.label>
-
-			<motion.input
-				id={"renene" + id}
-				ref={inputRef}
-				className="input"
-				type={showPassword ? "text" : type}
-				disabled={!enabled}
-				defaultValue={defaultValue}
-				onChange={onChange}
-				onFocus={onFocus}
-				onBlur={onBlur}
-				onInvalid={() => setValid(false)}
-				initial={{
-					borderWidth: 1,
-					borderColor: activeColor,
-				}}
-				animate={{
-					borderWidth:
-						isFocused || (!isValid && value.length > 0) ? 1 : 1,
-					borderColor: activeColor,
-				}}
-				transition={{
-					duration: 0.15,
-				}}
-			/>
-			<Icon
+            <motion.input
+                id={"renene" + id}
+                ref={inputRef}
+                className="input"
+                type={showPassword ? "text" : type}
+                disabled={!enabled}
+                defaultValue={defaultValue}
+                onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                onInvalid={() => setValid(false)}
+                initial={{
+                    borderWidth: 1,
+                    borderColor: activeColor,
+                }}
+                animate={{
+                    borderWidth:
+                        isFocused || (!isValid && value.length > 0) ? 1 : 1,
+                    borderColor: activeColor,
+                }}
+                transition={{
+                    duration: 0.15,
+                }}
+            />
+			<Icon iconName={"warning"} fill={colors.danger} style={{
+                    position: "absolute",
+                    right: "12px",
+                    top: "12px",
+                    display: showIcon ? "initial" : "none",
+                }}/>
+            <Icon
                 onTap={togglePassword}
-				style={{
-					position: "absolute",
-					right: "12px",
-					top: "12px",
-					display: type === "password" ? "initial" : "none",
-				}}
-				iconName={!showPassword ? "show" : "hide"}
-				fill={colors.grey700}
-				size={"24px"}
-			/>
-			<Frame
-				style={{paddingLeft: 16}}
-				width={"100%"}
-				height={32}
-				background={"transparent"}
-			>
-				<Type.Caption color={activeColor}>{message}</Type.Caption>
-			</Frame>
-		</StyledField>
-	);
+                style={{
+                    position: "absolute",
+                    right: "12px",
+                    top: "12px",
+                    display: type === "password" && !showIcon ? "initial" : "none",
+                }}
+                iconName={!showPassword ? "show" : "hide"}
+                fill={colors.grey700}
+                size={"24px"}
+            />
+            <Frame
+                style={{ paddingLeft: 16 }}
+                width={"100%"}
+                height={32}
+                background={"transparent"}
+            >
+                <Type.Caption color={activeColor}>{message}</Type.Caption>
+            </Frame>
+        </StyledField>
+    );
 }
 
 TextField.defaultProps = {
-	label: "Test",
-	type: "text",
-	enabled: true,
-	focused: false,
-	color: colors.primary900,
-	width: 375,
-	height: 70,
+    label: "Test",
+    type: "text",
+    enabled: true,
+    focused: false,
+    color: colors.primary900,
+    width: 375,
+    height: 70,
 };
 
 addPropertyControls(TextField, {
-	defaultValue: {
-		title: "Default Value",
-		type: ControlType.String,
-		defaultValue: "",
-	},
-	currentValue: {
-		title: "Current Value",
-		type: ControlType.String,
-		defaultValue: "",
-		hidden() {
-
-			return true
-		}
-	},
-	type: {
-		title: "Input Type",
-		type: ControlType.Enum,
-		options: ["text", "email", "password", "number"],
-		optionTitles: ["Text", "Email", "Password", "Number"],
-	},
-	label: {
-		title: "Label",
-		type: ControlType.String,
-	},
-	assistMessage: {
-		title: "Assist Msg",
-		type: ControlType.String,
-	},
-	errorMessage: {
-		title: "Error Msg",
-		type: ControlType.String,
-	},
-	enabled: {
-		title: "Enabled",
-		type: ControlType.Boolean,
-		defaultValue: true,
-	},
-	focused: {
-		title: "Focused",
-		type: ControlType.Boolean,
-		defaultValue: false,
-	},
-	empty: {
-		title: "Empty",
-		type: ControlType.Boolean,
-		defaultValue: false,
-		hidden() {
-			return true
-		}
-	},
-	color: {
-		title: "Color",
-		type: ControlType.Color,
-		defaultValue: colors.primary900,
-		hidden(props) {
-			return true;
-		},
-	},
+    defaultValue: {
+        title: "Default Value",
+        type: ControlType.String,
+        defaultValue: "",
+    },
+    currentValue: {
+        title: "Current Value",
+        type: ControlType.String,
+        defaultValue: "",
+        hidden() {
+            return true;
+        },
+    },
+    type: {
+        title: "Input Type",
+        type: ControlType.Enum,
+        options: ["text", "email", "password", "number"],
+        optionTitles: ["Text", "Email", "Password", "Number"],
+    },
+    label: {
+        title: "Label",
+        type: ControlType.String,
+    },
+    assistMessage: {
+        title: "Assist Msg",
+        type: ControlType.String,
+    },
+    errorMessage: {
+        title: "Error Msg",
+        type: ControlType.String,
+    },
+    enabled: {
+        title: "Enabled",
+        type: ControlType.Boolean,
+        defaultValue: true,
+    },
+    focused: {
+        title: "Focused",
+        type: ControlType.Boolean,
+        defaultValue: false,
+    },
+    empty: {
+        title: "Empty",
+        type: ControlType.Boolean,
+        defaultValue: false,
+        hidden() {
+            return true;
+        },
+    },
+    color: {
+        title: "Color",
+        type: ControlType.Color,
+        defaultValue: colors.primary900,
+        hidden(props) {
+            return true;
+        },
+    },
 });
